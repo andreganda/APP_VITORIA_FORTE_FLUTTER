e041966f670a1a12a9bbcac042e65e02253afc55
André Ganda <andre.ganda@pm.es.gov.br>
2021-09-18 19:44:07 -0300

    inicio da inclusão das fotos.

--- lib/pages/Perfil/perfil-page.dart
+++ lib/pages/Perfil/perfil-page.dart
@@ -1,11 +1,17 @@
 import 'dart:convert';
-
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:vitoria_forte/Model/Usuario.dart';
 import 'package:vitoria_forte/widget/menu-widget.dart';
 import 'package:vitoria_forte/widget/profile_widget.dart';
 import 'package:vitoria_forte/widget/textfield_widget.dart';
+
+import '../../constants.dart';
 
 class PerfilPage extends StatefulWidget {
   @override
@@ -14,6 +20,9 @@ class PerfilPage extends StatefulWidget {
 
 class _PerfilPageState extends State<PerfilPage> {
   Usuario userPage = new Usuario();
+  final picker = ImagePicker();
+  final ImagePicker _picker = ImagePicker();
+  File _imageFile;
 
   @override
   void initState() {
@@ -34,11 +43,8 @@ class _PerfilPageState extends State<PerfilPage> {
         padding: EdgeInsets.symmetric(horizontal: 32),
         physics: BouncingScrollPhysics(),
         children: [
-          ProfileWidget(
-            imagePath:
-                "https://sigeo.pm.es.gov.br//arquivos/relatorio_operacional/5BPM/relatorio_id_7135/14_9_2021_22_25_53_60_name_IMG-20210914-WA0022.jpg",
-            onClicked: () {},
-          ),
+          SizedBox(height: 24),
+          imageProfile(),
           SizedBox(height: 24),
           // Text(this.userPage.nome.toString()),
           TextFieldWidget(
@@ -68,6 +74,166 @@ class _PerfilPageState extends State<PerfilPage> {
         ],
       ),
     );
+  }
+
+  Widget imageProfile() {
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          builder: ((builder) => bottomSheet()),
+        );
+      },
+      child: Center(
+        child: Stack(children: <Widget>[
+          CircleAvatar(
+            radius:
+                80.0, //backgroundImage: AssetImage("asset/images/logo.png"),
+            backgroundImage: _imageFile == null
+                ? AssetImage("asset/images/logo.png")
+                : FileImage(File(_imageFile.path)),
+          ),
+          Positioned(
+            bottom: 20.0,
+            right: 20.0,
+            child: InkWell(
+              onTap: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: ((builder) => bottomSheet()),
+                );
+              },
+              child: Icon(
+                Icons.camera_alt,
+                color: Colors.teal,
+                size: 28.0,
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+
+  Widget bottomSheet() {
+    return Container(
+      height: 100.0,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 20,
+      ),
+      child: Column(
+        children: <Widget>[
+          Text(
+            "Escolha um lugar",
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            // ignore: deprecated_member_use
+            FlatButton.icon(
+              icon: Icon(Icons.camera),
+              onPressed: () {
+                takePhoto(ImageSource.camera);
+              },
+              label: Text("Camera"),
+            ),
+            // ignore: deprecated_member_use
+            FlatButton.icon(
+              icon: Icon(Icons.image),
+              onPressed: () {
+                takePhoto(ImageSource.gallery);
+              },
+              label: Text("Galeria"),
+            ),
+          ])
+        ],
+      ),
+    );
+  }
+
+  void takePhoto(ImageSource source) async {
+    final pickedFile = await _picker.getImage(
+      source: source,
+    );
+
+    File imageCropped;
+
+    if (pickedFile != null) {
+      File cropped = await ImageCropper.cropImage(
+        sourcePath: pickedFile.path,
+        aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
+        compressQuality: 100,
+        maxHeight: 700,
+        maxWidth: 700,
+        compressFormat: ImageCompressFormat.jpg,
+        androidUiSettings: AndroidUiSettings(
+          toolbarTitle: 'Camera',
+        ),
+        iosUiSettings: IOSUiSettings(
+          title: 'Camera',
+        ),
+      );
+
+      setState(() {
+        if (cropped != null) {
+          _imageFile = cropped;
+        } else {
+          _imageFile = File(pickedFile.path);
+        }
+
+        try {
+          Uint8List arrayBYtes;
+          String myPath = _imageFile.path;
+          _readFileByte(myPath).then((bytesData) {
+            arrayBYtes = bytesData;
+            String arrayBytesString = base64.encode(arrayBYtes);
+            print(arrayBytesString);
+          });
+        } catch (e) {
+          // if path invalid or not able to read
+          print(e);
+        }
+
+        var array = _readFileByte(_imageFile.path);
+        print(array.toString());
+        //_salvarFoto(array.to);
+        Navigator.pop(context);
+      });
+    }
+  }
+
+  // _salvarFoto(String base64) async {
+  //   final response = await http.post(
+  //     Uri.parse('${baseUrl}Usuario/salvar_foto_usuario'),
+  //     headers: <String, String>{
+  //       'Content-Type': 'application/json; charset=UTF-8',
+  //     },
+  //     body: jsonEncode(<String, String>{
+  //       'base64': base64,
+  //     }),
+  //   );
+
+  //   if (response.statusCode == 200) {}
+  // }
+
+  Future<Uint8List> _readFileByte(String filePath) async {
+    Uri myUri = Uri.parse(filePath);
+    File audioFile = new File.fromUri(myUri);
+    Uint8List bytes;
+    await audioFile.readAsBytes().then((value) {
+      bytes = Uint8List.fromList(value);
+      print('reading of bytes is completed');
+    }).catchError((onError) {
+      print('Exception Error while reading audio from path:' +
+          onError.toString());
+    });
+    return bytes;
   }
 
   _getUser() async {
