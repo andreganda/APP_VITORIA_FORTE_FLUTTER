f0388600df3c44add728725ecfdce6a37a84a682
Ranieri Gomes Couto Filho <Ranieri_couto@hotmail.com>
2021-10-23 13:20:18 -0300

    Vinculação ao end de dev e nome do aplicativo

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 const mainColor = Color(0xff2470c7);
-// const baseUrl = "http://vitoriaforte.web22f41.kinghost.net/api/";
+const baseUrl = "https://oaksolucoes.azurewebsites.net/api/";
+const baseUrlHub = "https://oaksolucoes.azurewebsites.net/chatHub";
+
 // const baseUrl = "https://oaksolucoes.com.br/api/";
 // const baseUrlHub = "https://oaksolucoes.com.br/chatHub";
-// const baseUrl = "http://oaksolucoes.web22f42.kinghost.net/api/";
 
-const baseUrl = "http://10.0.0.110:45455/api/";
-const baseUrlHub = "http://10.0.0.110:45455/chatHub";
+// const baseUrl = "http://10.0.0.110:45455/api/";
+// const baseUrlHub = "http://10.0.0.110:45455/chatHub";
